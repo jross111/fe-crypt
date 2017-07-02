b70b04bfa3709b92abbd8385d22003ef945f7aa1
github username <github email address>
2017-07-02 19:23:40 -0400

    nav bar

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import NavBar from './components/NavBar.js'
 
 class App extends Component {
   render() {
     return (
+      <div>
+      <NavBar />
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>      BITCH Fk U</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+        BITCH Fk U
         </p>
+      </div>
       </div>
     );
   }
---
+++ src/components/NavBar.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+
+function NavBar(props) {
+  return (
+    <nav className="navbar">
+      <div className="container-fluid">
+        <div className="navbar-header">
+          <h1> Hello From Navbar </h1>
+        </div>
+      </div>
+    </nav>
+  )
+}
+
+export default NavBar
